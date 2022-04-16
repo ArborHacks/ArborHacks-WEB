1d87cf29cb128320cf53d7e2887c0178b1fd85d4
wlshen123 <WesleyLShen@gmail.com>
2022-04-16 12:33:46 +0000

    added submission code (with bogus input)

--- src/AG/AG.js
+++ src/AG/AG.js
@@ -3,6 +3,22 @@ import './AG.css';
 class AG extends Component {
   postSubmission() {
     console.log("submitted!");
+    fetch('/api/upload', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      body: JSON.stringify({
+        field1: 'value1',
+        field2: 'value2'
+      })
+    })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   render = () => {
